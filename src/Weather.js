b05eb0451e9e221b53355aa24b68651f37b287fd
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-07-25 15:47:14 -0500

    CSS styling for the current temperature

--- src/Weather.js
+++ src/Weather.js
@@ -1,20 +1,23 @@
 import React from "react";
 
+import "./Weather.css";
+
 export default function Weather() {
   return (
     <div className="Weather">
       <main>
-        <div className="current-weather d-flex">
+        <div className="current-weather d-flex justify-content-between">
           <div>
             <h1>Paris</h1>
             <p>
-              <span id="current-date">Saturday 15:32</span>, moderate rain
+              <span>Saturday 15:32</span>, moderate rain
               <br />
               Humidity: <strong>87%</strong>, Wind: <strong>7.2km/h</strong>
             </p>
           </div>
-          <div className="currentTemp">
-            ☀️ <span>14</span>°C
+          <div className="currentTemp d-flex">
+            ☀️ <span className="tempDigits">14</span>
+            <span className="currentUnit">°C</span>
           </div>
         </div>
       </main>
