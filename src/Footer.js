9379355dfa50c89d307c31303f0b33426461c9c4
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-02 20:47:29 -0500

    Removed link in Footer

--- src/Footer.js
+++ src/Footer.js
@@ -24,14 +24,7 @@ export default function Footer() {
             {" "}
             on GitHub
           </a>{" "}
-          and{" "}
-          <a
-            href="https://monumental-sable-6adb63.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            hosted on Netlify
-          </a>
+          and hosted on Netlify
         </p>
       </footer>
     </div>
