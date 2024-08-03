b49edf68faabff28d9916b20812a3f8cf5b710ce
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-02 20:47:14 -0500

    Updated styling

--- src/WeatherUnit.js
+++ src/WeatherUnit.js
@@ -18,26 +18,31 @@ export default function WeatherUnit(props) {
   if (unit === `celsius`) {
     return (
       <div className="WeatherUnit">
-        <span className="tempDigits">{Math.round(props.temp)}</span>
-        <span className="currentUnit">
-          °C|
-          <a href="/" onClick={convertFahrenheit}>
-            ºF
-          </a>
-        </span>
+        <div className="currentTemperature">
+          <span className="tempDigits">{Math.round(props.temp)}</span>
+          <span className="currentUnit align-top">
+            °C |
+            <a href="/" onClick={convertFahrenheit}>
+              {" "}
+              ºF
+            </a>
+          </span>
+        </div>
       </div>
     );
   } else {
     let fahrenheit = Math.round((props.temp * 9) / 5 + 32);
     return (
       <div className="WeatherUnit">
-        <span className="tempDigits">{fahrenheit}</span>
-        <span className="currentUnit">
-          <a href="/" onClick={convertCelsius}>
-            ºC{" "}
-          </a>
-          | ºF
-        </span>
+        <div className="currentTemperature">
+          <span className="tempDigits">{fahrenheit}</span>
+          <span className="currentUnit align-top">
+            <a href="/" onClick={convertCelsius}>
+              ºC{" "}
+            </a>
+            | ºF
+          </span>
+        </div>
       </div>
     );
   }
